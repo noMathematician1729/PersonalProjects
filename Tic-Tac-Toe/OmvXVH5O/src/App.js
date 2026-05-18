a174b61d75588ae4fae85339861784e1fc241443
noMathematician1729 <yashmthakkar13@gmail.com>
2026-05-18 19:53:58 +0530

    180526

--- Tic-Tac-Toe/OmvXVH5O/src/App.js
+++ Tic-Tac-Toe/OmvXVH5O/src/App.js
@@ -6,9 +6,7 @@ function Square({value, onSquareClick}){
   );
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true)
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i){
     if(squares[i] || calculateWinner(squares)){
       return;
@@ -19,8 +17,7 @@ export default function Board() {
     }else{
       nextSquares[i]='O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares)
   }
   const winner = calculateWinner(squares);
   let status;
@@ -71,4 +68,24 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+export default function App(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  function handlePlay(nextSquares){
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+  return(
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
 }
